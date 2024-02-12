79dde32bbe25bef5c75a30e29ee7f686f81ba5f2
Marko Cupic <m.cupic@gmx.ch>
2024-02-12 23:12:25 +0100

    optimize the wording of altcha messages

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -27,7 +27,7 @@ $GLOBALS['TL_LANG']['ERR']['altcha_verification_failed'] = 'ALTCHA-Antispam-Übe
 $GLOBALS['TL_LANG']['ERR']['altcha_hmac_key_not_found'] = 'Der ALTCHA hmac key ist noch leer und muss konfiguriert werden. Bitte fügen Sie diesen in der Datei config/config.yaml hinzu. <a href="https://github.com/markocupic/contao-altcha-antispam?tab=readme-ov-file#configuration-and-usage" target="_blank">Mehr</a>';
 $GLOBALS['TL_LANG']['ERR']['altcha_widget_error'] = 'Verifizierung fehlgeschlagen. Versuchen Sie es später noch einmal.';
 $GLOBALS['TL_LANG']['ERR']['altcha_widget_footer'] = 'Geschützt durch <a href="https://altcha.org/" target="_blank">ALTCHA</a>';
-$GLOBALS['TL_LANG']['ERR']['altcha_widget_label'] = 'Verifizierung erfolgreich. Ich bin kein Bot.';
-$GLOBALS['TL_LANG']['ERR']['altcha_widget_verified'] = 'Überprüfung erfolgreich';
+$GLOBALS['TL_LANG']['ERR']['altcha_widget_label'] = 'Ich bin kein Bot.';
+$GLOBALS['TL_LANG']['ERR']['altcha_widget_verified'] = 'Ich bin ein Mensch.';
 $GLOBALS['TL_LANG']['ERR']['altcha_widget_verifying'] = 'Verifizierung...';
 $GLOBALS['TL_LANG']['ERR']['altcha_widget_waitAlert'] = 'Verifizierung... bitte warten.';
