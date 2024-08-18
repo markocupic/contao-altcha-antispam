ac8d04143b8b7097c7bfa221a8f54ba10330788e
fritzmg <fmg@inspiredminds.at>
2024-08-18 09:50:58 +0100

    fix routing for Contao 4.13

--- src/Controller/AltchaController.php
+++ src/Controller/AltchaController.php
@@ -20,7 +20,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/_contao_altcha_challenge', name: self::class)]
+#[Route(path: '/_contao_altcha_challenge', name: self::class)]
 class AltchaController extends AbstractController
 {
     public function __construct(
